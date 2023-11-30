978e4f781e16da643baf8179b73fc8af7a55426c
Rowan <r.kooij17@gmail.com>
2023-11-30 15:06:18 +0100

    Mapping van models in database

---
+++ HAN.OOSE.ICDE.Persistency.Database.Mapping/CourseMap.cs
@@ -0,0 +1,25 @@
+﻿using HAN.OOSE.ICDE.Persistency.Database.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAN.OOSE.ICDE.Persistency.Database.Mapping
+{
+    public class CourseMap : EntityMapper<Course>
+    {
+        public override void ConfigureExtension(EntityTypeBuilder<Course> builder)
+        {
+            builder.Property(x => x.StudyProgram);
+            builder.Property(x => x.Code);
+            builder.Property(x => x.CollegeYear);
+            builder.Property(x => x.CTE);
+
+            builder.HasMany(x => x.LearningOutcomeUnits).WithOne();
+            builder.HasMany(x => x.Competencies).WithOne();
+            builder.HasOne(x => x.CoursePlanning).WithOne();
+        }
+    }
+}
---
+++ HAN.OOSE.ICDE.Persistency.Database.Mapping/CoursePlanningMap.cs
@@ -0,0 +1,19 @@
+﻿using HAN.OOSE.ICDE.Persistency.Database.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAN.OOSE.ICDE.Persistency.Database.Mapping
+{
+    public class CoursePlanningMap : EntityMapper<CoursePlanning>
+    {
+        public override void ConfigureExtension(EntityTypeBuilder<CoursePlanning> builder)
+        {
+            builder.HasMany(x => x.Lessons).WithOne();
+            builder.HasMany(x => x.Examinations).WithOne();
+        }
+    }
+}
