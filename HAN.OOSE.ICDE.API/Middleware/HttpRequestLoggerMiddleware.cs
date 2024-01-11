1e415e5b218a93330150cdc647a024c98f642b39
Rowan <r.kooij17@gmail.com>
2024-01-11 19:25:39 +0100

    Logging middleware voor http requests, om te zien wat er binnenkomt

---
+++ HAN.OOSE.ICDE.API/Middleware/HttpRequestLoggerMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace HAN.OOSE.ICDE.API.Middleware
+{
+    public class HttpRequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<HttpRequestLoggerMiddleware> _logger;
+
+        public HttpRequestLoggerMiddleware(RequestDelegate next, ILogger<HttpRequestLoggerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = context.Request;
+
+            _logger.LogInformation($"Incomming Request: HTTP {request.Method} {request.Path}");
+
+            await _next(context);
+        }
+    }
+}
